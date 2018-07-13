c91bf56bab4dd227480cbd32a29d236bb4536bb1
Bayo Fodeke <bfodeke@gmail.com>
2018-07-13 00:01:50 -0400

    Added new component for conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,18 +2,57 @@ import React, { Component } from 'react';
 import './App.css';
 import Stock from './components/Stock.js';
 import LoginForm from './components/LoginForm';
+import Welcome from './components/Welcome';
 import StockSearch from './components/StockSearch';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false
+    }
+
+    this.loginStateToggle = this.loginStateToggle.bind(this);
+  }
+
+  loginStateToggle(event) {
+    event.preventDefault()
+
+    const isLoggedIn = this.state.isLoggedIn;
+    if (isLoggedIn == true) {
+      this.setState({
+        isLoggedIn: false
+      })
+    }
+    else {
+      this.setState({
+        isLoggedIn: true
+      })
+    }
+
+  }
+
   render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let login;
+
+    if (isLoggedIn) {
+      login = <Welcome />;
+    } else {
+      login = <LoginForm />
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Stockwatch</h1>
+          <form className="login-toggle" onSubmit={this.loginStateToggle}>
+            <button className="toggleAuth">Login/Out</button>
+          </form>
         </header>
         <div className="App-intro">
           <Stock />
-          <LoginForm />
+          {login}
           <StockSearch />
         </div>
       </div>
