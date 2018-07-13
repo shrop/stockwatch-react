d01a78d6c028c2b27bb84c209e032628096a0915
Bayo Fodeke <bfodeke@gmail.com>
2018-07-13 00:01:35 -0400

    Added component to showcase ‘logged in’ message

---
+++ src/components/Welcome.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+import LoginForm from './LoginForm';
+
+class Welcome extends Component {
+
+  render() {
+    return (
+      <h3>Welcome!</h3>
+    );
+  }
+}
+
+export default Welcome;
