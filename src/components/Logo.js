bc097355b1c61710a0e3dee00520313a717e0542
Bayo Fodeke <bfodeke@gmail.com>
2018-07-13 01:11:55 -0400

    Added various logo variations and added props
    This allows the component to be added while specifying the style of the returned logo.

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import '../App.css';
 import logoImage from '../assets/logo.svg';
+import logoImageInline from '../assets/logo-inline.svg';
+import logoImageMark from '../assets/logo-mark.svg';
 
 class Logo extends Component {
+  constructor(props) {
+    super(props)
+  }
 
   render() {
+    let logoPath;
+    if (this.props.style === 'mark') {
+      logoPath = logoImageMark
+    }
+    else if (this.props.style === 'inline') {
+      logoPath = logoImageInline
+    }
+    else {
+      logoPath = logoImage
+    }
+
     return (
-      <img src={logoImage} alt="Stockwatch Logo" />
+      <img className="stockwatch-logo" src={logoPath} alt="Stockwatch Logo" />
     );
   }
 }
