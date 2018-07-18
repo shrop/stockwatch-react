b0f40e4a0e1b054f1a61bd8f2cee6cef8402cc4a
Bayo Fodeke <bfodeke@gmail.com>
2018-07-18 08:07:07 -0400

    Added styles to component via css in emotionjs
    Also removed styles from main css

--- src/components/StockSearch.js
+++ src/components/StockSearch.js
@@ -1,5 +1,32 @@
 import React, { Component } from 'react';
 import '../App.css';
+import { css } from 'emotion'
+
+const stockList = css`
+  padding: 0;
+`
+
+const stockItem = css`
+  display: block;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  border-bottom: 1px solid #d9d9d9;
+`
+const stockPrice = css`
+  color: #ffffff;
+  background: #34CA6E;
+  padding: 0.5rem;
+  border-radius: 5px;
+  float: right;
+`
+const stockTitle = css`
+  color: #444444;
+  font-size: 1.5rem;
+`
+const stockName = css`
+  color: #999999;
+  font-size: 0.8rem;
+`
 
 class StockSearch extends Component {
 
@@ -61,15 +88,13 @@ class StockSearch extends Component {
         </form>
 
         <div className="stock-search__results">
-          <ul className="stock-search__results-list">
+          <ul className={stockList}>
             {this.state.matchingStocks.map((stock) => {
               return (
-                <li key={stock.id}>
-                  <div>{stock.attributes.title}</div>
-                  <div className="stock-search__results-list__company-meta">
-                    <div className="stock-search__results-list__company-meta__price">{stock.attributes.stock_price}</div>
-                    <h4>{stock.attributes.company_name}</h4>
-                  </div>
+                <li className={stockItem} key={stock.id}>
+                  <div className={stockPrice}>{stock.attributes.stock_price}</div>
+                  <div className={stockTitle}>{stock.attributes.title}</div>
+                  <div className={stockName}>{stock.attributes.company_name}</div>
                 </li>
               )
             })}
