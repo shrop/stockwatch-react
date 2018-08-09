7f6c7ac6ea06ad464b03d574235506c30c4bdd0f
Bayo Fodeke <bfodeke@gmail.com>
2018-08-08 20:26:44 -0400

    Renamed Welcome component

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import '../App.css';
+import Logo from './Logo';
+
+class Home extends Component {
+  render() {
+    return <Logo />;
+  }
+}
+
+export default Home;
