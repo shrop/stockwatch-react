e6022e46600770ba97c3917619940ec6ea706ff0
Bayo Fodeke <bfodeke@gmail.com>
2018-08-18 11:48:46 -0400

    Updated layout of login page with bootstrap

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col, PageHeader } from 'react-bootstrap';
+import Logo from './Logo';
 import '../App.css';
 
 class LoginForm extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       email: '',
       password: '',
+      buttonText: 'Log In'
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -27,18 +29,47 @@ class LoginForm extends Component {
     let buttonText;
 
     if (this.props.isLoggedIn === true) {
-      buttonText = 'Log Out';
-    }
-    else {
-      buttonText = 'Log In';
+      this.setState.buttonText = 'Log Out';
+    } else {
+      this.setState.buttonText = 'Log In';
     }
 
     return (
-      <div>
-        <form className="auth-form" onSubmit={this.props.action}>
-          <input className="auth-form__cta form-cta" type="submit" value={buttonText} />
-        </form>
-      </div>
+      <Grid>
+        <Row className="show-grid">
+          <Col>
+            <p className="text-center">
+              <Logo />
+            </p>
+          </Col>
+        </Row>
+        <hr />
+        <Row>
+          <Col xs={12}>
+            <h2>Log In</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={6}>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id
+              erat ut massa finibus fringilla eu et odio. Phasellus in erat at
+              lorem fermentum commodo vitae nec augue.
+            </p>
+          </Col>
+          <Col xs={12} md={6}>
+            <form className="auth-form" onSubmit={this.props.action}>
+              <p className="text-center">
+                <input
+                  className="btn btn-primary btn-hg"
+                  type="submit"
+                  value={this.state.buttonText}
+                />
+              </p>
+            </form>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
