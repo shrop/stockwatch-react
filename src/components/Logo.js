5bfcfd37f9996eac3725034a6b90c5c4add66cae
Bayo Fodeke <bfodeke@gmail.com>
2018-08-18 11:49:43 -0400

    Updated logo property names and updated to use states

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,29 +1,36 @@
 import React, { Component } from 'react';
-import '../App.css';
 import logoImage from '../assets/logo.svg';
 import logoImageInline from '../assets/logo-inline.svg';
-import { css } from 'emotion'
 import logoImageMark from '../assets/logo-mark.svg';
+import { css } from 'emotion';
 
-const logoStyles = css`
+let logoStyles = css`
   max-width: 100%;
-`
+`;
 
 class Logo extends Component {
-  render() {
-    let logoPath;
-    if (this.props.logoStyle === 'mark') {
-      logoPath = logoImageMark
-    }
-    else if (this.props.logoStyle === 'inline') {
-      logoPath = logoImageInline
-    }
-    else {
-      logoPath = logoImage
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      logoPath: logoImage,
+      logoAlt: 'StockWatch App - Drupal Contenta Demo'
+    };
 
+    // Select the correct size logo based on what prop value is passed in
+    // for 'logoVariant'.
+    if (this.props.logoVariant === 'mark') {
+      this.state.logoPath = logoImageMark;
+    } else if (this.props.logoVariant === 'inline') {
+      this.state.logoPath = logoImageInline;
+    }
+  }
+  render() {
     return (
-      <img className={logoStyles + ' stockwatch-logo'} src={logoPath} alt="Stockwatch Logo" />
+      <img
+        className={logoStyles + ' stockwatch-logo'}
+        src={this.state.logoPath}
+        alt={this.state.logoAlt}
+      />
     );
   }
 }
