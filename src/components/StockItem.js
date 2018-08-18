a0fc7fcb437e0a5415fe8775d1d47661b3b827d9
Bayo Fodeke <bfodeke@gmail.com>
2018-08-18 11:46:53 -0400

    Bootstrap markup plus styling updates.

--- src/components/StockItem.js
+++ src/components/StockItem.js
@@ -1,53 +1,38 @@
 import React, { Component } from 'react';
-import { css } from 'emotion'
+import { css } from 'emotion';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 const stockItem = css`
-  display: flex;
-  align-items: flex-start;
-  padding-top: .75rem;
-  padding-bottom: .75rem;
+  padding-top: 1.5rem;
+  padding-bottom: 1.75rem;
   border-bottom: 1px solid #d1d1d1;
-`
-const stockItemColumn1 = css`
-  flex-basis: 70%;
-`
-const stockItemColumn2 = css`
-  flex-basis: 30%;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-`
-const stockPrice = css`
-  font-size: 1.5rem;
-  text-align: right;
-`
+`;
 const stockSymbol = css`
   color: #444444;
-  font-size: 1.5rem;
-`
+  font-size: 3rem;
+  font-weight: 300;
+  line-height: 1.46428571;
+`;
 const stockName = css`
   color: #999999;
-  font-size: 0.8rem;
-  opacity: 0.7;
-`
+  font-size: 1.5rem;
+  font-weight: 300;
+`;
 const stockItemChange = css`
-  padding: 3px;
-  border-radius: 3px;
   display: inline-block;
-  color: #ffffff;
   float: right;
-  font-size: 0.8rem;
+  font-size: 1.5rem;
 
   &.positive {
-    background: #34CA6E;
+    color: #34ca6e;
   }
 
   &.negative {
-    background: #F46168;
+    color: #f46168;
   }
-`
-const stockItemChange__positive = stockItemChange + " positive";
-const stockItemChange__negative = stockItemChange + " negative";
+`;
+const stockItemChange__positive = stockItemChange + ' positive';
+const stockItemChange__negative = stockItemChange + ' negative';
 
 class StockItem extends Component {
   constructor(props) {
@@ -56,19 +41,22 @@ class StockItem extends Component {
 
   render() {
     return (
-      <div className={stockItem}>
-        <div className={stockItemColumn1}>
+      <Row className={stockItem}>
+        <Col xs={9}>
           <div className={stockSymbol}>{this.props.stock.attributes.title}</div>
-          <div className={stockName}>{this.props.stock.attributes.company_name}</div>
-        </div>
-
-        <div className={stockItemColumn2}>
-          <div className={stockPrice}>{this.props.stock.attributes.stock_price}</div>
-          <div>
-            <span className={stockItemChange__positive}>+2.91</span>
+          <div className={stockName}>
+            {this.props.stock.attributes.company_name}
           </div>
-        </div>
-      </div>
+        </Col>
+        <Col xs={3}>
+          <div className="pull-right">
+            <div className={stockSymbol}>
+              {this.props.stock.attributes.stock_price}
+            </div>
+            <div className={stockItemChange__positive}>+2.91</div>
+          </div>
+        </Col>
+      </Row>
     );
   }
 }
