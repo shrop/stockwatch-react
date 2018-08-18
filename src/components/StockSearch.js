e6d0714138319cfb554be1d18a6fcdf8ac317442
Bayo Fodeke <bfodeke@gmail.com>
2018-08-18 11:45:47 -0400

    Updated markup to use react-bootstrap

--- src/components/StockSearch.js
+++ src/components/StockSearch.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import '../App.css';
 import StockList from './StockList';
+import {
+  Grid,
+  Row,
+  Col,
+  PageHeader,
+  Button,
+  FormGroup,
+  InputGroup,
+  FormControl
+} from 'react-bootstrap';
 
 class StockSearch extends Component {
-
   constructor(props) {
     super(props);
+    this.stockEndPoint =
+      'https://stockwatch-api.shropnet.net/jsonapi/node/stock';
+
     this.state = {
       searchedStock: '',
       matchingStocks: []
@@ -13,6 +25,7 @@ class StockSearch extends Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.fetchStocks = this.fetchStocks.bind(this);
+    this.getAllStocks = this.getAllStocks.bind(this);
   }
 
   handleChange(event) {
@@ -20,51 +33,74 @@ class StockSearch extends Component {
       searchedStock: event.target.value
     });
   }
-
+  getAllStocks() {
+    fetch(this.stockEndPoint)
+      .then(res => res.json())
+      .then(result => {
+        if (result.data) {
+          return result.data;
+        } else {
+          return [];
+        }
+      });
+  }
   fetchStocks(event) {
     event.preventDefault();
 
     let searchStock = this.state.searchedStock;
-    let stockEndPoint = 'https://stockwatch-api.shropnet.net/jsonapi/node/stock';
-    let searchParams = 'filter[title][value]=' + searchStock + '&filter[title][operator]==';
-
+    let searchParams =
+      'filter[title][value]=' + searchStock + '&filter[title][operator]==';
+    let stockEndPointSearch = '';
     if (searchStock) {
-      stockEndPoint = stockEndPoint + '?' + searchParams;
+      stockEndPointSearch = this.stockEndPoint + '?' + searchParams;
     }
 
-    fetch(stockEndPoint)
+    fetch(stockEndPointSearch)
       .then(res => res.json())
-      .then(
-        (result) => {
-          console.log(result, 'Result')
-          if (result.data) {
-            this.setState({
-              matchingStocks: result.data
-            });
-          }
-          else {
-            this.setState({
-              matchingStocks: []
-            });
-          }
+      .then(result => {
+        if (result.data) {
+          this.setState({
+            matchingStocks: result.data
+          });
+        } else {
+          this.setState({
+            matchingStocks: []
+          });
         }
-      )
+      });
   }
 
   render() {
     console.log(this.state.matchingStocks);
     return (
-      <div className="stock-search container">
-        <h3>Stock Search</h3>
-        <form className="stock-search__form" onSubmit={this.fetchStocks}>
-          <input className="stock-search__form__input form-input" name="stock_search" type="text" placeholder="Search for stock(s) by symbol. E.g. AAPL,MSFT,VOD.L" value={this.state.searchedStock} onChange={this.handleChange} />
-          <input className="stock-search__form__cta form-cta" type="submit" value="Submit" />
-        </form>
+      <Grid>
+        <Row>
+          <Col>
+            <PageHeader>Stock Search</PageHeader>
+          </Col>
+        </Row>
+        <Row>
+          <form className="stock-search__form" onSubmit={this.fetchStocks}>
+            <FormGroup>
+              <InputGroup>
+                <FormControl
+                  type="text"
+                  placeholder="Search for stock(s) by symbol. E.g. AAPL,MSFT,VOD.L"
+                  value={this.state.searchedStock}
+                  onChange={this.handleChange}
+                />
+                <InputGroup.Button>
+                  <Button bsStyle="primary">Search</Button>
+                </InputGroup.Button>
+              </InputGroup>
+            </FormGroup>
+          </form>
 
-        <div className="stock-search__results">
-          <StockList stocks={this.state.matchingStocks} />
-        </div>
-      </div>
+          <div className="stock-search__results">
+            <StockList stocks={this.state.matchingStocks} />
+          </div>
+        </Row>
+      </Grid>
     );
   }
 }
