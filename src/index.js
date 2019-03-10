43121510bd4f933691336fe30cad906ca1f0c206
Bayo Fodeke <bfodeke@gmail.com>
2019-03-10 16:21:00 -0400

    Updated path since moving router to its own folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import StockWatchRouter from './components/StockWatchRouter';
+import StockWatchRouter from './components/StockWatchRouter/StockWatchRouter';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<StockWatchRouter />, document.getElementById('root'));
