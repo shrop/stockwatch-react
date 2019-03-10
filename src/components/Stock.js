9d1b91c542ace703aa9496c3ad0e1c0ceadaf447
Bayo Fodeke <bfodeke@gmail.com>
2019-03-10 16:23:25 -0400

    Added message about use

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import '../App.css';
 import JsonApi from 'devour-client';
 
+/**
+ * Not currently used but has code for reference.
+ */
 class Stock extends Component {
   constructor(props) {
     super(props);
