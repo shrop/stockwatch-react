95cebcc90f2016123d93a177b3584da5acdbc932
Bayo Fodeke <bfodeke@gmail.com>
2019-03-24 10:12:21 -0400

    Sending Bearer token with request to fetch stocks

--- src/components/DrupalAPI/DrupalAPI.js
+++ src/components/DrupalAPI/DrupalAPI.js
@@ -1,7 +1,16 @@
 import axios from 'axios';
+import qs from 'qs';
 
 const DrupalAPI = {
   serverAPI: 'https://stockwatch-api.shropnet.net/api/node/transaction',
+  getAccessToken() {
+    // TODO: Determine some way to get a refresh token if the current
+    // token has expired.
+    let sessionStatus = sessionStorage.getItem('oauthAccessToken');
+    if (sessionStatus) {
+      return sessionStatus;
+    }
+  },
   purchaseStockPromise(stock, price, quantity) {
     const transaction = {
       data: {
@@ -35,14 +44,20 @@ const DrupalAPI = {
     });
   },
   getMyStocksPromise() {
-    const myStocksEndpoint = this.serverAPI + '/?filter[transaction_owner]=8f208229-a56b-40b5-ae32-a7b77d74193e';
-    const data = {
-      'filter[transaction_owner][id]': '8f208229-a56b-40b5-ae32-a7b77d74193e'
-    }
+    // Still using hardcoded UUID to fetch by.
+    // TODO: Get UUID of the currently logged in user.
+    const queryData = qs.stringify({
+      filter: {
+        'transaction_owner.id': '8f208229-a56b-40b5-ae32-a7b77d74193e',
+      }
+    });
+    const myStocksEndpoint = this.serverAPI + '?' + queryData;
+
     return axios({
       method: 'GET',
       headers: {
-        "Content-Type": "application/vnd.api+json"
+        "Content-Type": "application/vnd.api+json",
+        "Authorization": "Bearer " + this.getAccessToken()
       },
       url: myStocksEndpoint,
     });
