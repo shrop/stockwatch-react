4f0d046d47d6a846574f13376a79774ea24a52db
Bayo Fodeke <bfodeke@gmail.com>
2019-03-30 18:23:33 -0400

    Fetching User UUID from Drupal on login
    Also log the user out if their accesstoken doesn’t fetch new data. We assume the token is expired

--- src/components/AppAuth/AppAuth.js
+++ src/components/AppAuth/AppAuth.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 export const appAuth = {
   isAuthenticated: false,
+  fetchUserDataEndpoint: 'https://stockwatch-api.shropnet.net/api',
   OAuthAppURL: 'https://stockwatch-api.shropnet.net',
   OAuthAppEndpoint: 'https://stockwatch-api.shropnet.net/oauth/authorize',
   OAuthAppSignOut: 'https://stockwatch-api.shropnet.net/user/logout?_format=json',
@@ -22,11 +23,16 @@ export const appAuth = {
       let oauthAccessToken = getParameterByName('access_token');
       sessionStorage.setItem('oauthAccessToken', oauthAccessToken);
       this.isAuthenticated = true;
+
+      // Fetch User Data. If this can't be fetched it means that the
+      // AccessToken is expired, therefore we'll nuke the stored token.
+      this.fetchUserData();
     }
     else {
       let sessionStatus = sessionStorage.getItem('oauthAccessToken');
       if (sessionStatus) {
         this.isAuthenticated = true;
+        this.fetchUserData();
       }
     }
   },
@@ -46,24 +52,41 @@ export const appAuth = {
 
     setTimeout(cb, 100);
   },
+  fetchUserData() {
+    const sessionStatus = sessionStorage.getItem('oauthAccessToken');
+
+    const self = this;
+    if (sessionStatus) {
+      axios({
+        method: 'GET',
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + sessionStatus
+        },
+        url: self.fetchUserDataEndpoint,
+      }).then(function(response){
+        try {
+          let userUUID = response.data.meta.links.me.meta.id;
+          sessionStorage.setItem('oauthUserId', userUUID);
+        } catch (error) {
+          // If we're here, the meta.links.me object doesn't exist in the
+          // response. So let's log the user out of the react side of things.
+          self.signout();
+        }
+
+      });
+    }
+  },
   signout(cb) {
     // Remove all local sessionStorage.
     let sessionStatus = sessionStorage.getItem('oauthAccessToken');
     if (sessionStatus) {
       sessionStorage.removeItem('oauthAccessToken');
-
+      sessionStorage.removeItem('oauthUserId');
       this.isAuthenticated = false;
     }
 
     setTimeout(cb, 100);
-
-    // Attempt to sign out of API also.
-    axios({
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json"
-      },
-      url: this.OAuthAppSignOut,
-    });
+    window.location.href = this.OAuthAppRedirectUri;
   }
 };
